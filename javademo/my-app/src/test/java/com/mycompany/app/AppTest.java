08f7f31a1906d0617bb493aaa26f01bafb1817aa
GiovanniCampanaro <161741780+GiovanniCampanaro@users.noreply.github.com>
2024-07-22 15:34:54 +0000

    correction javaMain

--- javademo/my-app/src/test/java/com/mycompany/app/AppTest.java
+++ javademo/my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -25,19 +25,8 @@ public class AppTest
         assertFalse( false );
     }
     @Test
-    public void testApp()
-    {
-        App.main(null);
-    }
-    @Test
-    public void testIstanza()
-    {
-        App pippo = new App();
-        assertNotNull(pippo);
-    }
-    @Test
     public void testAppMeteo()
     {
-        Main.main(null);
+        App.main(null);
     }
 }
